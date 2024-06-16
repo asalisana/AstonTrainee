a886cf0e93c3499a8026f276f4216a9399c52240
asalisana <alina.putrych@gmail.com>
2024-06-16 16:09:08 +0300

    initial commit

--- lection_5/homework.js
+++ lection_5/homework.js
@@ -1,33 +1,35 @@
 //1.
-// const a = {b: 1},
-//     c = Object.create(a);
-//
-// console.log(c.b); // ?
-// delete c.b;
-// console.log(c.b); // ?
-// delete a.b;
-// console.log(c.b); // ?
-// a.z = 2;
-// console.log(c.z); // ?
-// c.z = 3;
-// console.log(a.z); // ?
+const a = {b: 1},
+    c = Object.create(a);
+
+console.log(c.b); // 1
+delete c.b;
+console.log(c.b); // 1
+delete a.b;
+console.log(c.b); // undefined
+a.z = 2;
+console.log(c.z); // 2
+c.z = 3;
+console.log(a.z); // 2
 
 // 2.
 
-// const promise = new Promise(() => {
-// })
-// promise.prototype === Promise.__proto__ // ?
-//
-// const obj = {}
-// obj.__proto__ === Object.prototype // ?
-//
-// new Array([]).__proto__ === Array.prototype // ?
-//
-// function Fn1 () {}
-// function Fn2 () {}
-// Fn1.constructor === Fn2.constructor // ?
-//
-// Fn1.prototype === Fn2.prototype // ?
+const promise = new Promise(() => {
+})
+
+console.log(promise.prototype === Promise.__proto__) // false
+
+const obj = {}
+console.log(obj.__proto__ === Object.prototype) // true
+
+console.log(new Array([]).__proto__ === Array.prototype)// true
+
+function Fn1 () {}
+function Fn2 () {}
+
+console.log(Fn1.constructor === Fn2.constructor) // true
+
+console.log(Fn1.prototype === Fn2.prototype) // false
 //3.
 
 // У вас есть два конструктора, Animal и Bird.
@@ -41,12 +43,60 @@
 // Создайте объекты animal и bird с использованием соответствующих конструкторов и вызовите их методы speak и fly.
 // Решите задачу, используя прототипное наследование, чтобы Bird наследовал от Animal.
 
-// Должно быть такое поведение:
-// const animal = new Animal("Дженни");
-// const bird = new Bird("Воробей");
+//функция-конструктор
+function Animal(name) {
+    this.name = name;
+}
+
+Animal.prototype.speak = function (){
+    console.log("Some generic sound")
+}
+
+function Bird(name){
+    Animal.call(this, name)
+}
+
+Bird.prototype = Object.create(Animal.prototype);
+Bird.prototype.constructor = Bird;
+
+Bird.prototype.fly = function (){
+    console.log("Flying high!")
+}
+
+let animal =  new Animal('Ruby')
+let bird = new Bird('Jenny')
+
+console.log(animal.name)
+console.log(bird.name)
+animal.speak()
+bird.speak()
+bird.fly()
+
+
+//на классах
+// class Animal{
+//     constructor(name) {
+//         this.name = name;
+//     }
+//     speak(){
+//         console.log('something')
+//     }
+// }
 //
-// animal.speak(); // "Some generic sound"
-// bird.speak();   // "Some generic sound"
-// bird.fly();     // "Flying high!"
-
-
+// class Bird extends Animal{
+//     constructor(name) {
+//         super(name);
+//     }
+//     fly() {
+//         console.log('fly high')
+//     }
+// }
+//
+// let animal = new Animal('Ruby');
+// let bird = new Bird('Jenny')
+//
+// console.log(bird.name)
+// console.log(animal.name)
+// animal.speak()
+// bird.speak()
+// bird.fly()
